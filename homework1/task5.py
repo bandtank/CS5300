dfda314cd509e71e5375ee858e1b3d5cf4214d31
Anthony Andriano <anthony@andriano.xyz>
2025-02-01 22:40:42 +0000

    update task5 to match the new assignment

--- homework1/task5.py
+++ homework1/task5.py
@@ -1,13 +1,15 @@
 '''
-Create a list in Replit of your favorite books, including book titles and authors. Use list slicing to print the first three books in the list.
-
-Create a dictionary that represents a basic student database, including student names and their corresponding student IDs.
-
-Implement pytest test cases to verify the correctness of your code for each data structure.
+Assignment:
+1. Create a new file named task5.py and inside create a list of of your favorite books,
+   including book titles and authors.
+2. Use list slicing to print the first three books in the list.
+3. Create a dictionary that represents a basic student database, including student
+   names and their corresponding student IDs.
+4. Implement pytest test cases to verify the correctness of your code for each data structure.
 '''
 import pprint
 
-def run_task_5() -> None:
+def run_task5() -> None:
   print(slice_list())
   print()
 
@@ -17,11 +19,14 @@ def run_task_5() -> None:
     print(f"{student[0]:<10}  {student[1]:<10}")
 
 def slice_list() -> list:
-  books = [["The Great Gatsby", "Scott Fitzgerald"],
-           ["To Kill a Mockingbird", "Harper Lee"], ["1984", "George Orwell"],
-           ["Pride and Prejudice", "Jane Austen"],
-           ["The Catcher in the Rye", "J.D. Salinger"],
-           ["The Lord of the Rings", "J.R.R. Tolkien"]]
+  books = [
+    ["The Great Gatsby", "Scott Fitzgerald"],
+    ["To Kill a Mockingbird", "Harper Lee"],
+    ["1984", "George Orwell"],
+    ["Pride and Prejudice", "Jane Austen"],
+    ["The Catcher in the Rye", "J.D. Salinger"],
+    ["The Lord of the Rings", "J.R.R. Tolkien"]
+  ]
 
   return [x[0] for x in books[0:3]]
 
@@ -36,4 +41,4 @@ def student_database() -> dict:
   return students
 
 if __name__ == "__main__":
-  run_task_5()
+  run_task5()
