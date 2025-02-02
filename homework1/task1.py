f15aacd1e576504b305078b7a61007fe255f7573
Anthony Andriano <anthony@andriano.xyz>
2025-02-02 17:41:42 +0000

    add comments to task 1

--- homework1/task1.py
+++ homework1/task1.py
@@ -5,6 +5,10 @@ Assignment:
 3. Set up a pytest test case that verifies the output of your script.
 '''
 def run_task1() -> str:
+  ###
+  # Return the string instead of printing because
+  # testing is easier that way.
+  ###
   return "Hello, World!"
 
 if __name__ == "__main__":
--- homework1/tests/test_task1.py
+++ homework1/tests/test_task1.py
@@ -1,4 +1,4 @@
 import task1
 
 def test_task1():
-  assert task1.run_task1() == "Hello, World!"
+  assert task1.run_task1() == "Hello, World!" # The result is the expected value
