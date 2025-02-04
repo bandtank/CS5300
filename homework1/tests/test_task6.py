03f348f281438f40f431faa15792d1b25ecb410d
Anthony Andriano <anthony@andriano.xyz>
2025-02-04 16:19:35 -0700

    update task 6 to not need a local temp folder

--- homework1/tests/test_task6.py
+++ homework1/tests/test_task6.py
@@ -8,18 +8,14 @@ def test_task6_run_task6():
   assert result == "Word count: 104" # The result is the expected value
 
 def test_task6_count_words_in_file():
-  filename = "homework1/temp/word_count.task6.txt"
+  filename = "/tmp/task6_word_count.txt"
 
   with open(filename, 'w') as f:
     f.write("one two three, four five. six seven! eight, nine,  ten\neleven")
 
-  #Debug
-  #with open(filename, 'r') as f:
-  #  word_count = len(f.read().split())
-
   assert 11 == task6.count_words_in_file(filename) # The result is the expected value
 
-  fm.delete_files('homework1/temp/', '.task6.txt')
+  fm.delete_file(filename)
 
 '''
 Dynamically generate the name of a test based on the name of the input file.
--- homework1/utilities/file_manager.py
+++ homework1/utilities/file_manager.py
@@ -5,5 +5,8 @@ def delete_files(path: str, ext: str) -> None:
     if filename.endswith(ext):
       os.remove(os.path.join(path, filename))
 
+def delete_file(path: str) -> None:
+  os.remove(path)
+
 if __name__ == "__main__":
   delete_temp_files()
